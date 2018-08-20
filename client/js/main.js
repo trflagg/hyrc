0a74b110141ff2930f5a70e70c6dd81b91189fe5
Taylor Flagg <trflagg@gmail.com>
2018-08-19 21:36:23 -0400

    Use ReactDOM to render App component into root div in index.html

--- client/js/main.js
+++ client/js/main.js
@@ -1,1 +1,9 @@
-console.log('hello world!!!!')
+const ReactDOM = require('react-dom');
+
+const App = require('./components/app');
+
+ReactDOM.render(
+  App(),
+  document.getElementById('root')
+);
+

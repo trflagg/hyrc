dbcce5d167abfd9d04d64c236999231837263b0f
Taylor Flagg <trflagg@gmail.com>
2018-08-31 11:07:19 -0400

    Small improvements
    * sans-serif default font for input and error
    * Only log graphql query, variables, and result when not on production

--- client/js/graphql.js
+++ client/js/graphql.js
@@ -1,13 +1,18 @@
 import { request } from 'graphql-request';
 
 const BASE_URL = '/graphql';
+const isProduction = (process.env.NODE_ENV === 'production');
 
 export async function runQuery(query, variables = {}) {
-  console.log(`sending query: ${query}`);
-  console.log(`with variables: ${JSON.stringify(variables)}`);
+  if (!isProduction) {
+    console.log(`sending query: ${query}`);
+    console.log(`with variables: ${JSON.stringify(variables)}`);
+  }
 
   return request(BASE_URL, query, variables).then(data => {
-    console.log(`reply: ${JSON.stringify(data)}`);
+    if (!isProduction) {
+      console.log(`reply: ${JSON.stringify(data)}`);
+    }
     return data;
   });
 };

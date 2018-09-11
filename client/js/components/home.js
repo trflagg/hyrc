4c020fb931827901e66f6e782925ddd10911fa0f
Taylor Flagg <trflagg@gmail.com>
2018-09-11 12:10:04 -0400

    Improve home style

--- client/js/components/home.js
+++ client/js/components/home.js
@@ -2,13 +2,27 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import './home.scss';
+
 import Header from './header';
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <Link to="/messages">Edit Messages</Link>
+      <div id="home">
+
+        <table class='link-table'>
+          <thead>
+            <tr><td> Jump to page: </td></tr>
+          </thead>
+
+          <tbody>
+            <tr><td> <Link to="/messages">Edit Messages</Link> </td></tr>
+          </tbody>
+        </table>
+
+      </div>
     </div>
   )
 }

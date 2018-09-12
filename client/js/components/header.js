57618b770a91a16a3a1b2b5623dbe9db10abfd29
Taylor Flagg <trflagg@gmail.com>
2018-09-12 18:58:46 -0400

    Add navigation to header

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -2,11 +2,18 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+require('./header.scss');
+
 const Header = () => (
   <header>
     <Link to="/">
       <h1>hyrc</h1>
     </Link>
+    <div class='left-side'>
+      <Link to="/play">play</Link>
+      <Link to="/character">character</Link>
+      <Link to="/messages">messages</Link>
+    </div>
   </header>
 );
 

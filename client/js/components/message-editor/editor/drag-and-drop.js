080d9e30abdf59e74baf68e21610cf146badaf94
Taylor Flagg <trflagg@gmail.com>
2018-09-18 14:40:42 -0400

    Text with getGlobal template string convert to GlobalBlot in editor
    Drag and Drop editor parses set text and will convert getGlobal
    template string into GlobalBlot with global set based on template
    string by inserting blot into delta.
    
    Right now GlobalBlots all are FirstName but that will change.

--- client/js/components/message-editor/editor/drag-and-drop.js
+++ client/js/components/message-editor/editor/drag-and-drop.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Delta from 'quill-delta';
 
 const Quill = require('quill/dist/quill.min.js');
 require('quill/dist/quill.core.css');
@@ -27,7 +28,12 @@ class DragAndDropEditor extends React.Component {
 
   getText = () => {
     const contents = this.getContents();
+    // convert delta into text string
     const ops = _.get(contents, 'ops', []);
+    return this.deltaToText(ops);
+  }
+
+  deltaToText = ops => {
     let text = '';
     _.forEach(ops, operation => {
       if(operation.insert) {
@@ -36,6 +42,9 @@ class DragAndDropEditor extends React.Component {
             text = text + operation.insert;
             break;
           case 'object':
+            // only object right now is global blot
+            // TODO: check property to figure out what kind of blot and what
+            // template string to append
             text = text + `<%= avatar.getGlobal('${operation.insert.global}') %>`;
             break;
         }
@@ -50,8 +59,53 @@ class DragAndDropEditor extends React.Component {
 
   setText = text => {
     if (text) {
-      this.quill.setText(text.trimStart().trimEnd());
+      this.quill.setContents(this.textToDelta(text.trimStart().trimEnd()));
     }
+  }
+
+  textToDelta(passedInText) {
+    let text = passedInText;
+    let delta = new Delta;
+    // convert text into deltas by parsing template strings into blots
+    while(text !== '') {
+      // get start of next template
+      const nextStartIndex = text.indexOf('<%');
+      if (nextStartIndex === -1) {
+        // no template string, push the rest of the text
+        delta.insert(text);
+        text = '';
+      } else {
+        // push up to template string start
+        delta.insert(text.substring(0, nextStartIndex));
+        text = text.substring(nextStartIndex)
+        // get end of template string
+        // add 2 to include '%>'
+        const nextEndIndex = text.indexOf('%>') + 2;
+        if (nextEndIndex === -1) {
+          throw new Error(`Unterminated template string: ${text}`);
+        }
+        // convert template string to delta
+        const templateString = text.substring(0, nextEndIndex);
+        delta = this.modifyDeltaWithTemplateString(delta, templateString);
+        // remove template string
+        text = text.substring(nextEndIndex);
+      }
+    }
+    return delta;
+  }
+
+  modifyDeltaWithTemplateString(delta, templateString) {
+    // insert blot based on template string
+    const getGlobalRegEx = /getGlobal\(\'(\w+)\'\)/;
+    const getGlobalResults = getGlobalRegEx.exec(templateString);
+    if (_.get(getGlobalResults, 'length', 0) > 1) {
+      const globalName = getGlobalResults[1];
+      delta.insert({global: globalName});
+    } else {
+      // template string not handled, insert as text
+      delta.insert(templateString);
+    }
+    return delta;
   }
 
   handleFirstNameClick = () => {
